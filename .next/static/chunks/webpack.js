7b40e1e8798bc3d2210dda8b5b829f23604e8a9c
seem-less <mgirach@gmail.com>
2020-09-04 22:03:23 +0200

    tut 10 complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "dd877f79302c6905d681";
+/******/ 	var hotCurrentHash = "212b3d29934a33bd9ee3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Product.js
+++ components/Product.js
@@ -1,13 +1,18 @@
+import Link from 'next/link';
+
 const Product = (props) => {
 
     const {product} = props;
     return (
         <div className="card col-3 mb-3">
             <h3 className="card-header">{product.name}</h3>
-            <img src={product.image.sourceUrl} alt={product.name} />
+            <Link as={`/product/${product.slug}-${product.productId}`} href={`/product?slug=${product.slug}-${product.productId}`} >
+                <a>
+                    <img src={product.image.sourceUrl} alt={product.name} />
+                </a>
+            </Link>
             <div className="card-body">
                 <h6 className="card-subtitle text-center">{product.price}</h6>
-                <a href="" className="btn btn-primary">View</a>
             </div>
         </div>
     )
