327eca8b5d3a52a40b5b10633fd116c42bb79c17
seem-less <mgirach@gmail.com>
2020-09-10 20:49:19 +0200

    tut 15 complete

--- components/cart/AddToCartButton.js
+++ components/cart/AddToCartButton.js
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import {useState, useContext} from 'react';
 import { AppContext } from '../context/AppContext';
-import { addFirstProduct } from '../../functions';
+import { addFirstProduct, updateCart } from '../../functions';
 
 const AddToCartButton = ( props ) => {
 
     const {product} = props;
     const [ cart, setCart ] = useContext(AppContext);
+    const [ showViewCart, setShowViewCart] = useState(false);
 
     const handleAddToCartClick = () => {
         if(process.browser){
@@ -14,7 +15,12 @@ const AddToCartButton = ( props ) => {
 
             // If cart has item(s) already, then update the existing cart
             if( existingCart ){
+                existingCart = JSON.parse( existingCart );
+                const qtyToBeAdded = 1;
 
+                const updatedCart = updateCart( existingCart, product, qtyToBeAdded );
+
+                setCart( updatedCart );
             } else {
                 /**
                  *  add first product.
@@ -22,12 +28,19 @@ const AddToCartButton = ( props ) => {
                 const newCart = addFirstProduct(product);
                 setCart( newCart );
             }
+
+            setShowViewCart(true);
         }
     }
 
     return (
         <>
             <button onClick= { handleAddToCartClick } className="btn btn-secondary">Add to cart</button>
+            { showViewCart ? (
+                <Link href="/cart">
+                    <button className="black-seed-view-cart-btn btn btn-secondary">View Cart</button>
+                </Link>
+            ): ''}
         </>
     )
 }
--- functions.js
+++ functions.js
@@ -43,3 +43,82 @@ export const createNewProduct = (product, productPrice, qty) => {
         totalPrice: parseFloat((productPrice * qty).toFixed(2))
     }
 }
+
+export const updateCart = ( existingCart, product, qtyToBeAdded, newQty = false ) => {
+    const updatedProducts = getUpdatedProducts( existingCart.products, product, qtyToBeAdded, newQty);
+
+    const addPrice = (total, item) => {
+        total.totalPrice += item.totalPrice;
+        total.qty += item.qty;
+
+        return total;
+    }
+
+    // loop through the updated product array and add the totalPrice of each item to get the totalPrice.
+    let total = updatedProducts.reduce(addPrice, { totalPrice: 0, qty: 0 });
+
+    const updatedCart = {
+        products: updatedProducts,
+        totalProductsCount: parseInt(total.qty),
+        totalProductsPrice: parseFloat( total.totalPrice)
+    };
+
+    localStorage.setItem('blackseed-cart', JSON.stringify( updatedCart ));
+
+    return updatedCart;
+
+}
+
+/**
+ * Get updated products array,
+ * Updated the product if it exists
+ * and add the new product to the existing cart
+ * 
+ * @param {*} existingProductsInCart 
+ * @param {*} product 
+ * @param {*} qtyToBeAdded 
+ * @param {*} newQty 
+ */
+
+export const getUpdatedProducts = (existingProductsInCart, product, qtyToBeAdded, newQty = false) => {
+    const ProductExistsIndex = isProductInCart(existingProductsInCart, product.productId);
+
+    // if product exists (index of that product is found in the array), update the product quantity and totalPrice
+    if(-1 < ProductExistsIndex){
+        let updatedProducts = existingProductsInCart;
+        let updatedProduct = updatedProducts[ ProductExistsIndex ];
+
+        // If we have the new qty of the product available, set that else add the qtyToBeAdded
+        updatedProduct.qty = (newQty) ? parseInt(newQty) : parseInt(updatedProduct.qty + qtyToBeAdded);
+        updatedProduct.totalPrice = parseFloat(updatedProduct.price * updatedProduct.qty ).toFixed(2);
+        
+        return updatedProducts;
+    } else {
+        // if product not found push the new product to the existing products array.
+        let productPrice = getFloatVal( product.price );
+        const newProduct = createNewProduct( product, productPrice, qtyToBeAdded);
+        existingProductsInCart.push( newProduct );
+
+        return existingProductsInCart;
+    }
+}
+
+/**
+ * return index of the product if it exists
+ * 
+ * @param {*} existingProductsInCart 
+ * @param {*} productId 
+ * @return {number | *}
+ */
+
+export const isProductInCart = (existingProductsInCart, productId) => {
+    const returnItemThatExists = (item, index) => {
+        if( productId === item.productId ){
+            return item;
+        }
+    };
+
+    const newArray = existingProductsInCart.filter( returnItemThatExists );
+
+    return existingProductsInCart.indexOf( newArray[0] );
+}
