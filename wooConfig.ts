b22e9a9e24127d728eeaef2b68b95195baa8a794
seem-less <mgirach@gmail.com>
2020-10-20 10:58:47 +0200

    all migrated to ts

---
+++ wooConfig.ts
@@ -0,0 +1,8 @@
+const wooConfig = {
+    siteUrl: "https://www.kalonjimasi.club",
+    consumerKey: "ck_2fa9e2c74fc8ea5a38e8942fff9cf453b782dd84",
+    consumerSecret: "cs_f3c3491d82a4efe1aea38beffd35165ce2775a4d",
+    version: "wc/v3"
+}
+
+export default wooConfig;
